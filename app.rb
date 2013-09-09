0c1b645d8256bcb41e5033f9073bce0f5db35080
Emile Silvis <emile@silvis.co.za>
2013-09-09 12:57:23 +0200

    Clean up

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 #m41162520002
 #m47975403002
 
+#todo
+#change val of button while posting  $this.val("i'm finally done!"); // pfewww, that's was hard work!
+#disbale button while posting $inputs.prop("disabled", true);
+#spinjs? http://fgnass.github.io/spin.js/
+#background job https://github.com/faye/faye https://github.com/resque/resque
+
 require 'sinatra'
 require 'csv'
 require_relative 'lib/mxitbroadcast.rb'
@@ -11,13 +17,17 @@ get '/' do
 	erb :send
 end
 
-post '/' do
+post '/broadcast' do
 	ENV['MXIT_KEY'] = params[:mxit_key]
 	ENV['MXIT_SECRET'] = params[:mxit_secret]
 	ENV['MXIT_APP_NAME'] = params[:mxit_app_name]
 
-	csv = CSV.open(params[:recipients][:tempfile].path, :skip_blanks => true)
-	
+	#csv = CSV.open(params[:recipients][:tempfile].path, :skip_blanks => true)
+
+	#Net::HTTP.get(URI(params[:recipients]))
+
+	csv = CSV.parse(Net::HTTP.get(URI(params[:recipients])), :skip_blanks => true)
+
 	csv.each_slice(500) do |users|
 		begin
 			MxitAPI.send_message(users.join(','), params[:message])
@@ -26,12 +36,6 @@ post '/' do
 		end
 	end
 
-	redirect to ('/done')
-
-end
-
-get '/done' do
-	erb :done
 end
 
 error do
